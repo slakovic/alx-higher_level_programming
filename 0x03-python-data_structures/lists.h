87f0e8384068a77449254f2b1fb12c83e392ff66
slakovic <aladefm.o.a@gmail.com>
2022-12-05 13:21:48 +0100

    Task 0-print list integer

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,34 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
+
+#endif /* LISTS_H *
