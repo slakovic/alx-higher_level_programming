ae8bb8150648567aa57deb224a04826aa655220f
slakovic <aladefm.o.a@gmail.com>
2022-12-06 08:18:16 +0100

    Task 13-is palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
 typedef struct listint_s
 {
@@ -16,19 +15,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-def print_list_integer(my_list=[]):
-def element_at(my_list, idx):
-def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list=[]):
-def new_in_list(my_list, idx, element):
-def no_c(my_string):
-def print_matrix_integer(matrix=[[]]):
-def add_tuple(tuple_a=(), tuple_b=()):
-def multiple_returns(sentence):
-def max_integer(my_list=[]):
-def divisible_by_2(my_list=[]):
-def delete_at(my_list=[], idx=0):
-int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-#endif /* LISTS_H *
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
