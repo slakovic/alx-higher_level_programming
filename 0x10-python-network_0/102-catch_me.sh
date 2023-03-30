6b09b28f76c1bd28f22e6358d925ed6b89b17418
slakovic <aladefm.o.a@gmail.com>
2023-03-30 07:56:07 -0700

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Displays the body of the response of a curl POST request
-curl -sLX PUT -d "You got me!" -H "Origin:HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -d user_id=98 -H "You got me!" 0.0.0.0:5000/catch_me
 
