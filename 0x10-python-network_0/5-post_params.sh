9172f36bcec0cff249e989d9b3fa2c047b8d2c8f
slakovic <aladefm.o.a@gmail.com>
2023-03-30 07:31:39 -0700

    updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response, A variable email must be sent with the value hr@holbertonschool.com, A variable subject must be sent with the value I will always be here for PLD
- curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
-
+# Displays the body of the response of a curl POST request
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
